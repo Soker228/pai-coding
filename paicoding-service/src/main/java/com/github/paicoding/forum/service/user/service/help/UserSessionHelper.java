04bc801e9072b3f9002c8b89d76523f665475d25
lishangde <lishangde228@163.com>
2024-08-08 13:40:46 +0800

    更新注释

--- paicoding-service/src/main/java/com/github/paicoding/forum/service/user/service/help/UserSessionHelper.java
+++ paicoding-service/src/main/java/com/github/paicoding/forum/service/user/service/help/UserSessionHelper.java
@@ -59,12 +59,14 @@ public class UserSessionHelper {
     public String genSession(Long userId) {
         // 1.生成jwt格式的会话，内部持有有效期，用户信息
         String session = JsonUtil.toStr(MapUtils.create("s", SelfTraceIdGenerator.generate(), "u", userId));
-        String token = JWT.create().withIssuer(jwtProperties.getIssuer()).withExpiresAt(new Date(System.currentTimeMillis() + jwtProperties.getExpire()))
+        String token = JWT.create().withIssuer(jwtProperties.getIssuer())
+                .withExpiresAt(new Date(System.currentTimeMillis() + jwtProperties.getExpire()))
                 .withPayload(session)
                 .sign(algorithm);
 
         // 2.使用jwt生成的token时，后端可以不存储这个session信息, 完全依赖jwt的信息
-        // 但是需要考虑到用户登出，需要主动失效这个token，而jwt本身无状态，所以再这里的redis做一个简单的token -> userId的缓存，用于双重判定
+        // 但是需要考虑到用户登出，需要主动失效这个token，
+        // 而jwt本身无状态，所以再这里的redis做一个简单的token -> userId的缓存，用于双重判定
         RedisClient.setStrWithExpire(token, String.valueOf(userId), jwtProperties.getExpire() / 1000);
         return token;
     }
